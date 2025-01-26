2ecd14c2f2fe03d90dde24e30fba280114cc963c
DESKTOP-N598GAM\동호 <alu98@naver.com>
2025-01-26 23:58:47 +0900

    #41 View all list of notice
    view all list of notice

---
+++ src/main/java/com/infinity/isbbe/notice/repository/NoticeRepository.java
@@ -0,0 +1,12 @@
+package com.infinity.isbbe.notice.repository;
+
+import com.infinity.isbbe.notice.aggregate.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+
+}
---
+++ src/main/java/com/infinity/isbbe/notice/service/NoticeService.java
@@ -0,0 +1,9 @@
+package com.infinity.isbbe.notice.service;
+
+import com.infinity.isbbe.notice.dto.NoticeDTO;
+
+import java.util.List;
+
+public interface NoticeService {
+    List<NoticeDTO> getAllNotice();
+}
