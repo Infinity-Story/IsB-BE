56fb5562eb0c18f160dd1dc742802633b5de93e5
신동호 <101622086+letsplaycoding@users.noreply.github.com>
2025-01-30 21:19:05 +0900

    Jwt env

--- src/main/java/com/infinity/isbbe/admin/controller/AdminController.java
+++ src/main/java/com/infinity/isbbe/admin/controller/AdminController.java
@@ -45,11 +45,10 @@ public class AdminController {
     public ResponseEntity<String> createAdmin(@RequestBody ResponseAdmin request) {
         return adminService.createMember(request);
     }
-
     @Operation(summary = "관리자 수정", description = "기존 관리자를 수정합니다.")
     @PutMapping("/update/{adminCode}")
     public ResponseEntity<String> updateAdmin(@PathVariable int adminCode, @RequestBody ResponseAdmin request) {
         return adminService.updateAdmin(adminCode,request);
     }
-
+//hi
 }
